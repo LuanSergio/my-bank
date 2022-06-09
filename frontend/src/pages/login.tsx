2273baa9a13e466196ae2bd961ee071fd824acd8
Luan <luan.sergio@novahaus.com.br>
2022-06-09 00:50:02 -0300

    feat: add login page

---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,12 @@
+import LoginForm from '@components/LoginForm';
+import FormLayout from '@components/FormLayout';
+
+const Login = (): JSX.Element => {
+  return (
+    <FormLayout>
+      <LoginForm />
+    </FormLayout>
+  );
+};
+
+export default Login;

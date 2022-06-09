bcc32fa87f9889e2bb826dec2d5e8effbe4d07db
Luan <luan.sergio@novahaus.com.br>
2022-06-09 00:44:28 -0300

    chore: add sass and svg configuration to next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// next.config.js
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = {
+  esModule: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src', 'assets', 'styles')],
+  },
+  webpack: (config, { defaultLoaders }) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: { titleProp: true },
+        },
+      ],
+    });
+
+    return config;
+  },
+};

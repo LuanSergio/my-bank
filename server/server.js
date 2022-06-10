67af53b21738bd36d677d6469befe1a5f9171209
Luan <luan.sergio@novahaus.com.br>
2022-06-10 03:26:41 -0300

    fix: add cors fix to server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
+const cors = require("cors");
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ noCors: true });
 const port = process.env.PORT || 8080;
 
 server.db = router.db;
 
-server.use(auth);
+server.use(
+  cors({
+    origin: true,
+    credentials: true,
+    preflightContinue: false,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+server.options("*", cors());
 server.use(middlewares);
+server.use(auth);
 server.use(router);
 server.listen(port);

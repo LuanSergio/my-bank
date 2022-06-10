75affece0efbf2078ecbdb6510acd849f867980e
Luan <luan.sergio@novahaus.com.br>
2022-06-10 09:56:53 -0300

    chore: change form layout background

--- frontend/src/components/FormLayout/index.tsx
+++ frontend/src/components/FormLayout/index.tsx
@@ -2,6 +2,7 @@ import styles from './styles.module.scss';
 import RegisterForm from '@components/RegisterForm';
 import Header from '@components/Header';
 import { ReactNode } from 'react';
+import BackgroundDecoration from '../../../public/background-decoration.svg';
 
 interface IFormLayoutProps {
   children: ReactNode;
@@ -10,10 +11,11 @@ interface IFormLayoutProps {
 const FormLayout = ({ children }: IFormLayoutProps): JSX.Element => {
   return (
     <>
-      <div className={styles.content}>
-        <Header />
-        <main className={`h-container`}>{children}</main>
-      </div>
+      <Header />
+      <main className={` ${styles.content}`}>
+        <BackgroundDecoration className={styles.backgroundDecoration} />
+        {children}
+      </main>
     </>
   );
 };
